891dc0883f347dda3b860d31afcc8236f27481b2
anigvo <anigvo@gmail.com>
2023-11-28 23:58:16 +0200

    complete tasks

--- src/4.ts
+++ src/4.ts
@@ -1,5 +1,49 @@
-const key = new Key();
+class Key {
+    private signature: string;
+    constructor() {
+        this.signature = Math.random().toString();
+    }
+    getSignature(): string {
+        return this.signature;
+    }
+}
 
+class Person {
+    constructor(private key: Key) { 
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    constructor(public key: Key) {
+        this.key = key;
+    }
+    public door: boolean;
+    public tenants: Key[] = [];
+    public comeIn(person: Person) {
+        this.tenants.push(person.getKey());
+    }
+
+    public abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+    constructor(public key: Key) {
+        super(key)
+    }
+    openDoor(key: Key): boolean {
+        if (key.getSignature() === this.key.getSignature()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
@@ -8,4 +52,4 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
